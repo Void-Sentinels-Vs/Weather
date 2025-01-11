d3968802aff32907414bf02cd986b3c8303ecdc7
pryntl <pryntl2007@gmail.com>
2025-01-11 23:58:35 +0330

    fix : js
    removed the unnecessary things

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
-document.addEventListener("DOMContentLoaded", getTime)
+document.addEventListener("DOMContentLoaded", getTime);
 
+function getTime() {
+  var currentDate = new Date();
+  var hours = currentDate.getHours();
 
-function getTime (){ 
+  let bg = document.getElementById("backGround");
 
-
-    var currentDate = new Date();
-    var hours = currentDate.getHours();
-    
-
-    let bg = document.getElementById("backGround")
-    
-    if (hours >= 6 && hours < 18 ){
-         bg.setAttribute('src' , 'images/day.jpg')
-    }else if (hours >= 18 && hours < 24){
-        bg.setAttribute('src' , 'images/midday.jpg')
-    }else if ( hours < 6){ 
-        bg.setAttribute('src' , 'images/night.jpg')
-    
-    }
-    
-     
-
-
-    document.getElementById("imageContainer").appendChild(imgElement);
-
-    
-    alert("کاربر ساعت " + hours + ":" + minutes + " وارد صفحه شد.");
-};    
-
-
+  if (hours >= 6 && hours < 18) {
+    bg.setAttribute("src", "images/day.jpg");
+  } else if (hours >= 18 && hours < 24) {
+    bg.setAttribute("src", "images/midday.jpg");
+  } else if (hours < 6) {
+    bg.setAttribute("src", "images/night.jpg");
+  }
+}
