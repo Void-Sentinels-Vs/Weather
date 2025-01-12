28160b74d17e7f358724a18d919975cde175baa3
pryntl <pryntl2007@gmail.com>
2025-01-12 12:09:39 +0330

    feat js
    put a condition for the right section image. it'll set due to the time

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ function getTime() {
   var hours = currentDate.getHours();
 
   let bg = document.getElementById("backGround");
-
+  let section = document.getElementById("section")
   if (hours >= 6 && hours < 18) {
     bg.setAttribute("src", "images/day.jpg");
+    section.setAttribute("src", "images/rightSection1.jpg");
   } else if (hours >= 18 && hours < 24) {
     bg.setAttribute("src", "images/midday.jpg");
+    section.setAttribute("src", "images/rightSection2.jpg");
   } else if (hours < 6) {
     bg.setAttribute("src", "images/night.jpg");
+    section.setAttribute("src", "images/rightSection3.jpg");
   }
 }
