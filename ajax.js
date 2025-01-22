a99389bbe4720e7d2f6df70e9e70b16ce18d3aff
Hadixp <hadi.b.2002@gmail.com>
2025-01-22 09:22:44 +0330

    Change
    I changed api from xhr request to fetch api

--- ajax.js
+++ ajax.js
@@ -1,23 +1,41 @@
 //hadixp
 // this file is for getting weather data from api
 
+// function loadData(location){
+//     const xhr = new XMLHttpRequest();
+//     const apiKey = 'd4f16e53afe04283826224619251001';
+//     const endPoint = 'https://api.weatherapi.com/v1/current.json';
+//     const Location = `${location}`;
+//     const url = `${endPoint}?key=${apiKey}&q=${Location}`;
+//     xhr.open('GET',url);
+//     xhr.onload = function(){
+//         const dataApi = JSON.parse(this.response);
+//         // console.log(dataApi.current);
+//         tempResult.textContent = dataApi.current.temp_c + '°C';
+//         humidity.textContent = dataApi.current.humidity + '%';
+//         rain.textContent = dataApi.current.precip_in + 'in';
+//         uv.textContent = dataApi.current.uv + 'UV';
+//     }
+//     xhr.send();
+// }
+
+// now usuing fetch
 function loadData(location){
-    const xhr = new XMLHttpRequest();
     const apiKey = 'd4f16e53afe04283826224619251001';
     const endPoint = 'https://api.weatherapi.com/v1/current.json';
     const Location = `${location}`;
     const url = `${endPoint}?key=${apiKey}&q=${Location}`;
-    xhr.open('GET',url);
-    xhr.onload = function(){
-        const dataApi = JSON.parse(this.response);
-        // console.log(dataApi.current);
-        tempResult.textContent = dataApi.current.temp_c + '°C';
-        humidity.textContent = dataApi.current.humidity + '%';
-        rain.textContent = dataApi.current.precip_in + 'in';
-        uv.textContent = dataApi.current.uv + 'UV';
-    }
-    xhr.send();
+    fetch(url).then((res)=>{
+        res.json().then((dataApi)=>{
+            console.log(dataApi);
+            tempResult.textContent = dataApi.current.temp_c + '°C';
+            humidity.textContent = dataApi.current.humidity + '%';
+            rain.textContent = dataApi.current.precip_in + 'in';
+            uv.textContent = dataApi.current.uv + 'UV';
+        })
+    })
 }
+
 
 // button & input selection
 const btn = document.querySelector('.btn');
